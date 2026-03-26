71a1e78c115917f231464d3e35d952aab74374ae
Steffi-art19 <stepka.odstrc@gmail.com>
2026-03-26 10:26:32 +0000

    init

--- src/main.cpp
+++ src/main.cpp
@@ -3,23 +3,24 @@
 
 int main()
 {
-    std::cout << "Vitejte v kalkulacce" << std::endl;
-    std::cout << "Zadejte prvni cislo: ";
+    std::cout << "Zadejte svuj vek: ";
+
+    std::string input;
+    std::cin >> input;
+
+    std::string output;
+    output = "Je ti " + input + " let.";
+
+    std::cout << output << std::endl;
+
+    std::cout << "Zacina druhy program" << std::endl;
+
+    std::cout << "Zadejte cislo: ";
     float a;
     std::cin >> a;
 
-    float square = a * a;
-    std::cout << "Druha mocnina cisla " << a << " je " << square << std::endl;
+    float b = a * a;
+    std::cout << "Druha mocnina cisla " << a << " je " << b << std::endl;
 
-    std::cout << "Zacina treti program" << std::endl;
-
-    double radius;
-    std::cout << "Zadejte polomer: ";
-    std::cin >> radius;
-    const double pi = 3.1415;
-    double circumference = 2 * pi * radius;
-    double area = pi * radius * radius;
-    std::cout << "Obvod kruhu je " << circumference << " cm a jeho obsah je " << area << " cm2." << std::endl;
-    
     return 0;
 }
