cd7bec2f4b16a12f56902960c80ad1f3b10be850
Steffi-art19 <stepka.odstrc@gmail.com>
2026-04-16 10:09:03 +0000

    funkce

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,15 @@
 #include <iostream>
-#include <vector>
-#include <array>
+
+void sayHi(); // deklarace
 
 int main()
 {
-
-    std::array<std::array<int, 3>, 2> matrix = {{{1, 2, 3},
-                                                 {4, 5, 6}}};
-   
-
-    for (size_t i = 0; i < matrix.size(); i++)
-    {
-        for (size_t j = 0; j < matrix[i].size(); j++)
-            std::cout << matrix[i][j] << " ";
-
-        std::cout << std::endl;
-    }
-
+    sayHi(); // OK
+  
     return 0;
+}
+
+void sayHi() // definice
+{
+    std::cout << "Hi!" << std::endl;
 }
