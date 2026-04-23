05fe03481ca2d10937c3cf2c01a09e6f9f2c22f2
Steffi-art19 <stepka.odstrc@gmail.com>
2026-04-23 11:31:43 +0000

    struktury a tridy

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,34 @@
 #include <iostream>
+#include <string>
 
-void sayHi(); // deklarace
+class User
+{
+public:
+    User(const std::string& name, int age, const std::string& street)
+        : name_(name), age_(age), street_(street)
+    {
+    }
+
+    void printUser() const
+    {
+        std::cout << "Name: " << name_ << std::endl;
+        std::cout << "Age: " << age_ << std::endl;
+        std::cout << "Street: " << street_ << std::endl << std::endl;
+    }
+
+private:
+    std::string name_;
+    int age_;
+    std::string street_;
+};
 
 int main()
 {
-    sayHi(); // OK
-  
-    return 0;
-}
+    User user1("Tomas Marny", 33, "Sikma 5");
+    User user2("Josef Novy", 28, "Ve Svahu 8");
 
-void sayHi() // definice
-{
-    std::cout << "Hi!" << std::endl;
+    user1.printUser();
+    user2.printUser();
+
+    return 0;
 }
